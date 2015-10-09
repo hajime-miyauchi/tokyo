8498604a227485075e88030462203dc27afe905b
hajime-miyauchi <hajime@hajime-no-MacBook-Pro.local>
2015-10-09 12:33:29 +0900

    名古屋から渋谷へのルートを追加

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,7 +1,7 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 //kml読み込み
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://hajime-miyauchi.github.io/tokyo/cesium-starter-app-master/Source/test.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://hajime-miyauchi.github.io/tokyo/cesium-starter-app-master/Source/nagoya2shibuya.kml'));
 
 //視点の変更
-viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
+viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 150.0)});
