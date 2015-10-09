9f7217c7931405faca46d36c6742180da1cfbdff
hajime-miyauchi <hajime@hajime-no-MacBook-Pro.local>
2015-10-09 12:37:26 +0900

    視点を変更

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -4,4 +4,4 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 viewer.dataSources.add(Cesium.KmlDataSource.load('http://hajime-miyauchi.github.io/tokyo/cesium-starter-app-master/Source/nagoya2shibuya.kml'));
 
 //視点の変更
-viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 150.0)});
+viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(138.384155, 35.056608, 150000.0)});
